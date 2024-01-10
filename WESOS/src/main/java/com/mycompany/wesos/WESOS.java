e008a7e1e7a4a29ebdfcd34eb3d2332b5a0cf56c
Eclodest <156086194+Eclodest@users.noreply.github.com>
2024-01-10 12:19:57 +0100

    Add files via upload

---
+++ WESOS/src/main/java/com/mycompany/wesos/WESOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wesos;
+
+/**
+ *
+ * @author Ilerna 4.2.3
+ */
+public class WESOS {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO XDXDXDXDXD");
+    }
+}
